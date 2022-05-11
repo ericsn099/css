89e5c6e6a625a983bdccd19bee394a40ae72373e
Eric Negreiros <ericsn099@hotmail.com>
2022-05-10 20:48:58 -0400

    cadastro_ocorrencia

--- index.php
+++ index.php
@@ -1,38 +1,220 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <link href="./meuCss/myStyle2.css" rel="stylesheet" />
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>Vigilante</title>
+    <title>Pesquisar Ocorrência</title>
+    <style>
+        .n-like {
+            margin-right: 10px;
+            font-size: 20px;
+        }
+        .area-do-post form .op-post {
+            display: flex;
+            flex-direction: row;
+        }
+        .btn{
+            width: 150px;
+        }
+    </style>
 </head>
 
 <body>
+    <?php
+
+    $pequisa = $_POST['busca'] ?? '';
+
+    include "conexao.php";
+    $sql = "SELECT publicacaoagente.datahora, publicacaoagente.id,bairro.nomeBairro, rua.nome, publicacaoagente.nivel, publicacaoagente.publicacao from bairro,rua,publicacaoagente  where bairro.id = publicacaoagente.id_bairro and rua.id = publicacaoagente.id_rua and bairro.nomeBairro like '%$pequisa%' order by publicacaoagente.id desc";
+    $dados = mysqli_query($conn, $sql);
+    ?>
+
 
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class="jumbotron">
-                    <h1 class="display-4">Community System Security</h1>
-                    <p class="lead">Sistema de fiscalização de crimes em bairros de Manaus</p>
-                    <hr class="my-4">
-                    <p>Você pode se cadastrar para ser um agente.</p>
-                    <a class="btn btn-primary btn-lg" href="login.php" role="button">Entrar</a>
-                    <a class="btn btn-primary btn-lg" href="pesquisarOcorrencia.php" role="button">Pesquisar</a>
+                <nav class="navbar navbar-light bg-light col-6">
+                    <div class="container-fluid ">
+                        <a class="navbar-brand">Ocorrências</a>
+                        <form class="d-flex" action="" method="POST">
+                            <input class="form-control me-2" type="search" placeholder="Bairro" aria-label="Search" name="busca">
+                            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+                        </form>
+                    </div>
+                </nav>
+                <div class="dashbord">
+                    <div class="dash-item">
+                        <div class="dash-info">
+                        <h3>Asssalto</h3>
+                            <?php $sql2 = "SELECT COUNT(id_situacao) as soma FROM publicacaoagente WHERE id_situacao = 1";
+                            $dados3 = mysqli_query($conn, $sql2);
+                            ?>
+                            <?php
+                            while ($linha = mysqli_fetch_assoc($dados3)) {
+                                $nome = $linha['soma'];
+                                echo "<div class='qtd'> $nome </div>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="dash-item">
+                        <div class="dash-info">
+                            <h3>Furto</h3>
+                            <?php $sql2 = "SELECT COUNT(id_situacao) as soma FROM publicacaoagente WHERE id_situacao = 2";
+                            $dados3 = mysqli_query($conn, $sql2);
+                            ?>
+                            <?php
+                            while ($linha = mysqli_fetch_assoc($dados3)) {
+                                $nome = $linha['soma'];
+                                echo "<div class='qtd'> $nome </div>";
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="dash-item">
+                        <div class="dash-info">
+                            <h3>Tentativa de Asssalto</h3>
+                            <?php $sql2 = "SELECT COUNT(id_situacao) as soma FROM publicacaoagente WHERE id_situacao = 3";
+                            $dados3 = mysqli_query($conn, $sql2);
+                            ?>
+                            <?php
+                            while ($linha = mysqli_fetch_assoc($dados3)) {
+                                $nome = $linha['soma'];
+                                echo "<div class='qtd'> $nome </div>";
+                            }
+                            ?>
+                        </div>
+                        
+                    </div>
+                    <div class="dash-item">
+                        <div class="dash-info">
+                            <h3>Suspeitos</h3>
+                            <?php $sql2 = "SELECT COUNT(id_situacao) as soma FROM publicacaoagente WHERE id_situacao = 4";
+                            $dados3 = mysqli_query($conn, $sql2);
+                            ?>
+                            <?php
+                            while ($linha = mysqli_fetch_assoc($dados3)) {
+                                $nome = $linha['soma'];
+                                echo "<div class='qtd'> $nome </div>";
+                            }
+                            ?>
+                        </div>
+
+                    </div>
                 </div>
-                
+
+                <div class="area-ocorrencia">
+                    <div class="area1">
+                        <div class="area1-area">
+                            <div class="cabecacho-user">
+                                <img src="img/im1.png" alt="">
+                                <div class="user-name">Usuario logado</div>
+                            </div>
+                            <div class="user-opcoes">
+                                <div class="opcoes-icon">
+                                    <i class="fa-solid fa-house"></i>
+                                    <div class="icon-titulo">
+                                        <a href="">Home</a>
+                                    </div>
+                                </div>
+                                <div class="opcoes-icon">
+                                    <i class="fa-solid fa-plus"></i>
+                                    <div class="icon-titulo">
+                                        <a href="">Ocorrência</a>
+                                    </div>
+                                </div>
+                                <div class="opcoes-icon">
+                                    <i class="fa-brands fa-rocketchat"></i>
+                                    <div class="icon-titulo">
+                                        <a href="">Chat</a>
+                                    </div>
+                                </div>
+                                <div class="opcoes-icon">
+                                    <i class="fa-solid fa-rectangle-xmark"></i>
+                                    <div class="icon-titulo">
+                                        <a href="">Sair</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="area2">
+                        <?php
+
+                        while ($pub = mysqli_fetch_assoc($dados)) {
+                            $qtdLike = 0;
+                            if (isset($_POST['like'])) {
+                                $qtdLike++;
+                            }
+                            /*  if( isset( $_POST['mes'] ) ) {
+                                $mes = 0 + $_POST['mes'];
+                             } else {
+                                $mes = 0 + date('m');
+                             }*/
+
+                            $id = $pub['id'];
+                            $bairro = $pub['nomeBairro'];
+                            $rua = $pub['nome'];
+                            $nivel = $pub['nivel'];
+                            $publicacao = $pub['publicacao'];
+                            $datahora = $pub['datahora'];
+
+                            if ($nivel == 1) {
+                                $nivel  = "<div class='nivel' style='background: green'> </div>";
+                            } else if ($nivel == 2) {
+                                $nivel  = "<div class='nivel' style='background: yellow'> </div>";
+                            } else {
+                                $nivel  = "<div class='nivel' style='background: red'> </div>";
+                            }
+
+                            echo "<div class='ocorrencia'>
+                                            <div class='cab-oc'>
+                                                <h3> $bairro, $rua <br> <font size='4px'>$datahora</font> </h3>
+                                                   $nivel
+                                                   
+                                            </div>
+                                            
+                                            <div class='corpo-oc'>$publicacao</div>
+                                            <div class='area-do-post'>
+                                               
+                                                <div class='op-post'>
+                                                    <form method='POST'> 
+                                                        <span class='n-like'>$qtdLike</span>
+                                                        <button name='like'><i class= 'fa-solid fa-thumbs-up'></i></button>
+                                                    </form>
+                                                    <span>Curti</span>
+                                                </div>
+                                                <div class='op-post'>
+                                                <i class= 'fa-solid fa-message'></i>
+                                                    <span>Comentar </span>
+                                                </div>
+                                                <div class='op-post'>
+                                                <i class='fa-solid fa-file-import'></i>
+                                                    <span>Compartilhar </span>
+                                                </div>
+                                              
+                                            </div>
+                                      </div>";
+                        }
+                        ?>
+
+
+                    </div>
+                </div>
             </div>
         </div>
+
+        <button class="qualquer">Clica aqui</button>
     </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="js/like.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
--- index1.php
+++ index1.php
@@ -14,21 +14,20 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="row-9">
-            <?php
-                include "conexao.php";
 
-                $publicacao = $_POST['publicacao'];
-                $nivel = $_POST['nivel'];
-                $situacao = $_POST['cmocorrencia'];
-                $bairro = $_POST['cmbairros'];
-                $rua = $_POST['cmrua'];
-                $sql = "INSERT INTO `publicacaoagente`(`publicacao`, `nivel`, `id_situacao`,`datahora`,`id_agente`, `id_bairro`, `id_rua`) 
-                        VALUES ('$publicacao','$nivel','$situacao',now(),'1','$bairro','$rua')";
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="jumbotron">
+                    <h1 class="display-4">Community System Security</h1>
+                    <p class="lead">Sistema de fiscalização de crimes em bairros de Manaus</p>
+                    <hr class="my-4">
+                    <p>Você pode se cadastrar para ser um agente.</p>
+                    <a class="btn btn-primary btn-lg" href="login.php" role="button">Entrar</a>
+                    <a class="btn btn-primary btn-lg" href="pesquisarOcorrencia.php" role="button">Pesquisar</a>
+                </div>
                 
-                mysqli_query($conn, $sql);
-            ?>
+            </div>
         </div>
     </div>
 
@@ -38,12 +37,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    <script>
-        window.location.replace("http://localhost/Community-System-Security-mai/Ocorrencia.php");
-    </script>
-
+    -->
 </body>
 
 </html>
