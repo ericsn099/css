d30b961a4702cd46eb8ff26371f23e000d998e1b
Eric Negreiros <ericsn099@hotmail.com>
2022-05-13 23:12:32 -0400

    vai

--- conexao.php
+++ conexao.php
@@ -1,14 +1,14 @@
 <?php
-   /* $server = "sql105.epizy.com";
+    $server = "sql105.epizy.com";
     $user = "epiz_31720665";
     $pass = "zFLwDjPU8yUUf";
-    $bd = "epiz_31720665_sistemaCss";*/
+    $bd = "epiz_31720665_sistemaCss";
   
-    $server = "localhost";
+  /*$server = "localhost";
     $user = "root";
     $pass = "19019407eric@";
     $bd = "sistemacc";
-  
+  */
   
     $conn = mysqli_connect($server,$user,$pass,$bd);
 ?>
