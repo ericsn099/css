ec1ef843276d63c93da81170d213f27336c717ad
Eric Negreiros <ericsn099@hotmail.com>
2022-05-13 21:25:40 -0400

    vai

--- conexao.php
+++ conexao.php
@@ -1,6 +1,9 @@
 <?php
-    
+    $server = "sql105.epizy.com";
+    $user = "epiz_31720665";
+    $pass = "zFLwDjPU8yUUf";
+    $bd = "epiz_31720665_sistemaCss";
+  
+    mysqli_connect($server,$user,$pass,$bd);
 
-   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli("sql105.epizy.com", "epiz_31720665", "zFLwDjPU8yUUf", "epiz_31720665_sistemaCss");
 ?>
