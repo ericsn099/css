db4b36d669dff498e726e7b5c25b89c4b1d52e6f
Eric Negreiros <ericsn099@hotmail.com>
2022-05-13 22:48:24 -0400

    vai

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $user = "hkdv87nksr44e5ie";
-    $pass = "xcs72ym8llbtdbit";
-    $bd = "fub405ag7o3sn8yd";
+    $server = "sql105.epizy.com";
+    $user = "epiz_31720665";
+    $pass = "zFLwDjPU8yUUf";
+    $bd = "epiz_31720665_sistemaCss";
   /*
     $server = "localhost";
     $user = "root";
